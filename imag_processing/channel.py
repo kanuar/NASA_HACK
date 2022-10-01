740aa987fd0f03154d110a145cd1a7045e540743
Aayush Senapati <aayushsenapati2002@gmail.com>
2022-10-02 02:36:33 +0530

    Path issues fixed

--- imag_processing/channel.py
+++ imag_processing/channel.py
@@ -1,8 +1,9 @@
-import cv2 as cv,numpy as np
-imgb=cv.imread("C:\\Users\\aayus\\college\\NASA\\ImageSet\\JNCE_2022229_44C00045_V01-blue.png",cv.IMREAD_UNCHANGED)
-imgg=cv.imread("C:\\Users\\aayus\\college\\NASA\\ImageSet\\JNCE_2022229_44C00045_V01-green.png",cv.IMREAD_UNCHANGED)
-imgr=cv.imread("C:\\Users\\aayus\\college\\NASA\\ImageSet\\JNCE_2022229_44C00045_V01-red.png",cv.IMREAD_UNCHANGED)
-map=cv.imread("C:\\Users\\aayus\\college\\NASA\\ImageSet\\JNCE_2022229_44C00045_V01-mapprojected.png",cv.IMREAD_UNCHANGED)
+import cv2 as cv
+import numpy as np
+imgb=cv.imread("C:\\Users\\aayus\\college\\NASA_HACK\\imag_processing\\ImageSet\\JNCE_2022229_44C00045_V01-blue.png",cv.IMREAD_UNCHANGED)
+imgg=cv.imread("C:\\Users\\aayus\\college\\NASA_HACK\\imag_processing\\ImageSet\\JNCE_2022229_44C00045_V01-green.png",cv.IMREAD_UNCHANGED)
+imgr=cv.imread("C:\\Users\\aayus\\college\\NASA_HACK\\imag_processing\\ImageSet\\JNCE_2022229_44C00045_V01-red.png",cv.IMREAD_UNCHANGED)
+map=cv.imread("C:\\Users\\aayus\\college\\NASA_HACK\\imag_processing\\ImageSet\\JNCE_2022229_44C00045_V01-mapprojected.png",cv.IMREAD_UNCHANGED)
 mergetemp=cv.merge([imgb,imgg,imgr])
 
 #add(or blend) the images
@@ -15,7 +16,6 @@ imgr=cv.resize(imgr,(300,300))
 map=cv.resize(map,(300,300))
 mergetemp=cv.resize(mergetemp,(300,300))
 mergefinal=cv.resize(mergefinal,(300,300))
-88564423
 
 cv.imshow('mergetemp',mergetemp)
 cv.imshow('mergefinal',mergefinal)
