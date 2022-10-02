fee22e6e4b0ad455fba963b3d82891915e813284
raunak kodwani <68312266+kanuar@users.noreply.github.com>
2022-10-02 09:52:36 +0530

    Create downloader.py

---
+++ api/downloader.py
@@ -0,0 +1,8 @@
+def downloader(k):
+    wget.download(k)
+    f1=str(i)+'-Data.zip'
+    f2=str(i)+'-ImageSet.zip'
+    with ZipFile(f1,'r') as zipobj:
+        zipobj.extractall()
+    with ZipFile(f2,'r') as zipobj:
+        zipobj.extractall()
